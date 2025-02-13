d6f4d64839787c02b7b5d53f128f26a845191a38
Dragon <115075047+Dragonbestgg1@users.noreply.github.com>
2025-02-13 16:52:29 +0200

    Update

--- app/addExam/page.tsx
+++ app/addExam/page.tsx
@@ -4,10 +4,36 @@ import Header from '@/components/ui/Header'
 import style from '@/styles/pages/addExam.module.css'
 
 export default function AddExam() {
-    return(
+    return (
         <div className={`${style.main}`}>
             <Header />
-            <h1>Add Exam</h1>
+            <form className={`${style.form}`}>
+                <div className={`${style.mainInfo}`}>
+                    <div className={`${style.input}`}>
+                        <label htmlFor="examName">Exam Name</label>
+                        <input type="text" id="examName" />
+                    </div>
+                    <div className={`${style.input}`}>
+                        <label htmlFor="examDate">Exam Date</label>
+                        <input type="date" id="examDate" />
+                    </div>
+                    <div className={`${style.input}`}>
+                        <label htmlFor="examTime">Exam Time</label>
+                        <input type="time" id="examTime" />
+                    </div>
+                    <div className={`${style.input}`}>
+                        <label htmlFor="examDuration">Exam Duration</label>
+                        <input type="number" id="examDuration" />
+                    </div>
+                    <div className={`${style.input}`}>
+                        <label htmlFor="examType">Exam Type</label>
+                        <select id="examType">
+                            <option value="1">Online</option>
+                            <option value="2">Offline</option>
+                        </select>
+                    </div>
+                </div>
+            </form>
         </div>
     )
 }
